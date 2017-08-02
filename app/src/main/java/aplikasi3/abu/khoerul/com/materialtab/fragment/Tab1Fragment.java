faebb888c5d819eb244dce54d58ba76dc81fb21e
abhu66 <khoirulabhu@gmail.com>
2017-08-02 23:04:48 +0700

    tambah folder fragment 2 file fragment

---
+++ app/src/main/java/aplikasi3/abu/khoerul/com/materialtab/fragment/Tab1Fragment.java
@@ -0,0 +1,30 @@
+package aplikasi3.abu.khoerul.com.materialtab.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import aplikasi3.abu.khoerul.com.materialtab.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Tab1Fragment extends Fragment {
+
+
+    public Tab1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab1, container, false);
+    }
+
+}
